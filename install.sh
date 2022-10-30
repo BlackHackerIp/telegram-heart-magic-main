d47980b615dd17e83328540bb7ccf2faeed58730
BlackHackerIp <115902527+BlackHackerIp@users.noreply.github.com>
2022-10-31 01:00:13 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@ echo "|     Кто ты ?     |"
 echo "|------------------|"
 echo "| 1. Termux        |"
 echo "| 2. kali linux    |"
+echo "| 3. Переустановить|"
 echo "|                  |"
-echo "| Введите: 1/2     |"
+echo "| Введите: 1/2/3   |"
 echo "--------------------"
 read numb
 if [ $numb = "1" ]
@@ -14,20 +15,25 @@ then
 	pkg install python
 	pkg install python3
 	pip install telethon
-	cd ~/telegram-heart-magic-main/magic_heart.py
+	cd ~/telegram-heart-magic-main
 	python3 magic_heart.py
 else
 	if [ $numb = "2" ]
-	then
-		if [ "$(whoami)" != 'root' ];
 		then
 			echo "У вас нет прав. Запустите install.sh с root правами"
 			exit
 		else
 			apt install python3 python3-pip
             pip3 install telethon
-	        cd ~/telegram-heart-magic-main/magic_heart.py
+	        cd ~/telegram-heart-magic-main
 	        python3 magic_heart.py
+		fi
+	else
+		if [ $numb = "3" ] 
+		then
+        rm -r ~/telegram-heart-magic-main
+		git clone https://github.com/BlackHackerIp/telegram-heart-magic-main
+		cd ~/telegram-heart-magic-main
 
 		else
 			echo "Некорректный ввод"
